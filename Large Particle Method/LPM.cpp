0dc30c418d9ccf0b82b3cc65145b6568e6227ab5
BelyaevPavel <gbelbih@gmail.com>
2016-09-17 15:05:20 +0600

    Triangle and stat wave correct(One phase).

--- Large Particle Method/LPM.cpp
+++ Large Particle Method/LPM.cpp
@@ -12,7 +12,6 @@ LPM::LPM()
 	gamma = 1.4;
 	lambda1 = 0.026;
 	p0 = 0.1e6;
-	a10 = 341;
 	M = 4.2;
 	mu1 = 1.85e-5;
 	T0 = 293;
@@ -23,9 +22,12 @@ LPM::LPM()
 	ls = 3;
 	lw = 3.5;
 	//Gas calculated parameters initialisation
-	u1f = 2 / (gamma + 1)*(M - 1 / M)*a10;
-	p1 = (1 + 2 * gamma / (gamma + 1)*M*M - 1)*p0;
+	p1 = (1 + 2 * gamma*(M*M - 1) / (gamma + 1))*p0;//(1 + 2 * gamma / (gamma + 1)*M*M - 1)*p0;
 	ro01f = (gamma + 1)*M*M / (2 + (gamma - 1)*M*M)*ro01;
+	a10 = sqrt(gamma*p0 / ro01);
+	a1 = sqrt(gamma*p1 / ro01f);
+	u1f = 2 / (gamma + 1)*(M - 1 / M)*a10;
+	e0 = p0 / (ro01*(gamma - 1));
 	//Auxiliary variables initialisation
 	a = 0.9;
 	b = 0.5;
@@ -35,10 +37,10 @@ LPM::LPM()
 	tstop = 0.01;
 	//Grid parameters initialisation
 	N = 400;
-	Co = 0.1;
+	Co = 0.3;
 	dx = lw / N;
-	dt = Co*dx / (u1f*u1f + a10*a10);
-	//Arrays declaration initialisation
+	dt = Co*dx / (sqrt(u1f*u1f + a10*a10));
+	//Arrays initialisation
 	U = new double[N];
 	UIntermediate = new double[N];
 	U1 = new double[N];
@@ -53,26 +55,30 @@ LPM::LPM()
 	P1 = new double[N];
 	Ro = new double[N];
 	Ro1 = new double[N];
-	//Auxiliary arrays declaration initialisation
+	//Auxiliary arrays initialisation
 	viscosity1 = new double[N];
 	viscosity2 = new double[N];
+	RightBorderMassFlow = new double[N];
 
 	for (int i = 0; i < N; i++)
 	{
 		if (i*dx < lf)
 		{
-			U[i] = u1f / (i*dx);
-			xi = pow(1 - (gamma - 1)*(u1f - U[i]) / (2 * a10), 2 / (gamma - 1));
+			V[i] = u1f * (i*dx) / lf;
+			xi = pow(1 - (gamma - 1)*(u1f - V[i]) / (2 * a10), 2 / (gamma - 1));
 			P[i] = p1*pow(xi, gamma);
 			Ro[i] = ro01f*xi;
-			E[i] = P[i] / ((gamma - 1)*Ro[i]) + U[i] * U[i] / 2.0;
+			//V[i] = u1f;
+			//P[i] = p1;
+			//Ro[i] = ro01f;
+			E[i] = P[i] / ((gamma - 1)*Ro[i]) + V[i] * V[i] / 2.0;
 		}
 		else
 		{
-			U[i] = 0;
+			V[i] = 0;
 			P[i] = 100000;
 			Ro[i] = ro01;
-			E[i] = P[i] / ((gamma - 1)*Ro[i]);
+			E[i] = e0;
 		}
 	}
 }
@@ -96,11 +102,11 @@ double LPM::DefineFlowDirection(int index, double* IntermVelocity, int Border, d
 	}
 }
 
-double LPM::DefineMassTranslatio(double ThisCellVelocity, double RightCellVelocity, double ThisCellDensity, double RightCellDensity, double dt)
+double LPM::DefineRightBorderMassTranslation(double ThisCellVelocity, double RightCellVelocity, double ThisCellDensity, double RightCellDensity, double dt)
 {
 	if (ThisCellVelocity + RightCellVelocity > 0)
 	{
-		return ThisCellDensity*(ThisCellDensity + RightCellVelocity)*dt / 2.0;
+		return ThisCellDensity*(ThisCellVelocity + RightCellVelocity)*dt / 2.0;
 	}
 	else
 	{
@@ -118,29 +124,104 @@ double LPM::DefineMassTranslatio(double ThisCellVelocity, double RightCellVeloci
 
 void LPM::MainProc()
 {
-	// Artificial viscosity calculation.
-	for (int i = 2; i < N - 1;i++)
+	double* temp;
+	int n = this->N;
+	while (t <= tstop)
 	{
-		if (V[i + 1]<V[i])
+		loopCounter++;
+		// Artificial viscosity calculation.
+		for (int i = 1; i < N - 1; i++)
 		{
-			viscosity1[i] = ((Ro[i + 1] + Ro[i]) / 2)*(a*sqrt(1.4*(P[i + 1] + P[i]) / (Ro[i + 1] + Ro[i])) + b*(V[i] - V[i + 1]))*(V[i] - V[i + 1]);
-			viscosity1[i] = 0;
+			if (V[i + 1]<V[i])
+				viscosity1[i] = ((Ro[i + 1] + Ro[i]) / 2)*(a*sqrt(1.4*(P[i + 1] + P[i]) / (Ro[i + 1] + Ro[i])) + b*(V[i] - V[i + 1]))*(V[i] - V[i + 1]);
+			else
+				viscosity1[i] = 0;
+
+			if (V[i - 1]>V[i])
+				viscosity2[i] = -((Ro[i - 1] + Ro[i]) / 2)*(a*sqrt(1.4*(P[i - 1] + P[i]) / (Ro[i - 1] + Ro[i])) - b*(V[i] - V[i - 1]))*(V[i] - V[i - 1]);
+			else
+				viscosity2[i] = 0;
 		}
-		if (V[i - 1]>V[i])
+		viscosity1[0] = viscosity1[1];
+		viscosity1[N - 1] = viscosity1[N - 2];
+		viscosity2[0] = viscosity2[1];
+		viscosity2[N - 1] = viscosity2[N - 2];
+		// Intermediate pressure calculation.
+		for (int i = 0; i < N - 1; i++)
 		{
-			viscosity2[i] = -((Ro[i - 1] + Ro[i]) / 2)*(a*sqrt(1.4*(P[i - 1] + P[i]) / (Ro[i - 1] + Ro[i])) - b*(V[i] - V[i - 1]))*(V[i] - V[i - 1]);
+			PIntermediate[i] = (P[i + 1] + P[i]) / 2.0;
+			if (PIntermediate[i] < 0)
+				cout << loopCounter << "Fail." << endl;
 		}
-		else
-			viscosity2[i] = 0;
+		PIntermediate[N - 1] = PIntermediate[N - 2];
+		// Intermediate velocity calculation.
+		for (int i = 1; i < N - 1; i++)
+		{
+			VIntermediate[i] = V[i] - ((P[i + 1] - P[i - 1]) / 2.0 + viscosity1[i] - viscosity2[i])*(dt / (dx*Ro[i]));
+
+			//if (VIntermediate[i] < 0)
+			//	cout << loopCounter << "Fail." << endl;
+		}
+		VIntermediate[0] = VIntermediate[1];
+		VIntermediate[N - 1] = -VIntermediate[N - 2];
+		// Intermediate energy calculation
+		for (int i = 1; i < N - 1; i++)
+		{
+			EIntermediate[i] = E[i] - ((PIntermediate[i] + viscosity1[i])*(V[i] + V[i + 1]) / 2 - (PIntermediate[i - 1] + viscosity2[i])*(V[i] + V[i - 1]) / 2)*(dt / (dx*Ro[i]));
+
+			if (EIntermediate[i] < 0)
+				cout << loopCounter << "Fail." << endl;
+		}
+		EIntermediate[0] = EIntermediate[1];
+		EIntermediate[N - 1] = EIntermediate[N - 2];
+		// Calculation of mass flow through right cell border
+		for (int i = 0; i < N - 1; i++)
+			RightBorderMassFlow[i] = DefineRightBorderMassTranslation(V[i], V[i + 1], Ro[i], Ro[i + 1], dt);
+		RightBorderMassFlow[N] = 0;
+		// Next time step density calculation
+		for (int i = 1; i < N; i++)
+			Ro1[i] = Ro[i] + (RightBorderMassFlow[i - 1] - RightBorderMassFlow[i]) / dx;
+		Ro1[0] = Ro1[1];
+		// Final calculations cycle
+		for (int i = 1; i < N - 1; i++)
+		{
+			LeftCellVelocityFlow = DefineFlowDirection(i - 1, VIntermediate, 1, VIntermediate);
+			LeftCellEnergyFlow = DefineFlowDirection(i - 1, VIntermediate, 1, EIntermediate);
+			ThisCellEnergyFlow = DefineFlowDirection(i, VIntermediate, 1, EIntermediate);
+			ThisCellVelocityFlow = DefineFlowDirection(i, VIntermediate, 1, VIntermediate);
+			E1[i] = (Ro[i] * dx) / (Ro1[i] * dx)*EIntermediate[i] + (1.0 / (Ro1[i] * dx))*(LeftCellEnergyFlow * RightBorderMassFlow[i - 1] - ThisCellEnergyFlow * RightBorderMassFlow[i]);
+			V1[i] = (Ro[i] * dx) / (Ro1[i] * dx)*VIntermediate[i] + (1.0 / (Ro1[i] * dx))*(LeftCellVelocityFlow * RightBorderMassFlow[i - 1] - ThisCellVelocityFlow * RightBorderMassFlow[i]);
+			P1[i] = Ro1[i] * (E1[i] - V1[i] * V1[i] / 2.0)*0.4;
+			//if (V1[i] < 0)
+			//	cout << loopCounter << "Fail." << endl;
+		}
+		E1[0] = E1[1];
+		E1[N - 1] = E1[N - 2];
+		P1[0] = P1[1];
+		P1[N - 1] = P1[N - 2];
+		V1[0] = V1[1];
+		V1[N - 1] = -V1[N - 2];
+		Ro1[N - 1] = Ro1[N - 2];
+
+		temp = E1;
+		E1 = E;
+		E = temp;
+
+		temp = P1;
+		P1 = P;
+		P = temp;
+
+		temp = V1;
+		V1 = V;
+		V = temp;
+
+		temp = Ro1;
+		Ro1 = Ro;
+		Ro = temp;
+
+		t += dt;
+		if (loopCounter % 100 == 0)
+			cout << loopCounter << " " << t << endl;
 	}
-	// Intermediate pressure calculation.
-	for (int i = 0; i < N-1; i++)
-		PIntermediate[i] = (P[i + 1] + P[i]) / 2.0;
-	PIntermediate[N - 1] = PIntermediate[N - 2];
-	// Intermediate velocity calculation.
-	for (int i = 2; i < N - 1; i++)
-		VIntermediate[i] = V[i] - ((P[i + 1] - P[i - 1]) / 2.0 + viscosity1[i] - viscosity2[i])*(dt / (dx*Ro[i]));
-	VIntermediate[0] = VIntermediate[1];
-	VIntermediate[N - 1] = VIntermediate[N - 2];
 
 }
--- Large Particle Method/LPM.h
+++ Large Particle Method/LPM.h
@@ -1,23 +1,27 @@
 #pragma once
 #include <math.h>
+#include <iostream>
+
+using namespace std;
 class LPM
 {
 	//Gas and particles constant parameters
-	double R;
-	double cp;
-	double c2;
-	double ro01;
-	double ro02;
-	double gamma;
-	double lambda1;
-	double p0;
-	double a10;
-	double M ;
-	double mu1;
-	double T0;
+	double R; //Gas constant
+	double cp; //Gas heat capacity at constant pressure
+	double c2; //Particles material heat capacity
+	double ro01; //Gas density
+	double ro02; //Particles density
+	double gamma; //Adiabatic exponent
+	double lambda1; //Thermal conduction
+	double p0; //Pressure stagnation value
+	double a10; //Sound speed stagnation value
+	double a1; //
+	double M; //Mach
+	double mu1; //Viscousity
+	double T0; //Temperature stagnation value
 	//Shielding layer constant parameters
-	double alpha10;
-	double d;
+	double alpha10; //Gas content percente
+	double d; //Particle's diameter
 	double lf;
 	double ls;
 	double lw;
@@ -25,39 +29,45 @@ class LPM
 	double u1f;
 	double p1;
 	double ro01f;
+	double e0;
 	//Auxiliary variables
 	double a;
 	double b;
 	double xi;
-	double loopCounter;
-	double t;
-	double tstop;
+	int loopCounter;
+	double t; //Current time
+	double tstop; //Calculation stop time
+	double LeftCellEnergyFlow;
+	double LeftCellVelocityFlow;
+	double ThisCellEnergyFlow;
+	double ThisCellVelocityFlow;
 	//Grid parameters
-	int N;
-	double Co;
-	double dx;
-	double dt;
+	int N; //Grid cells count
+	double Co; //Courant 
+	double dx; //Grid step
+	double dt; //Time step
 	//Arrays declaration
-	double *U;
-	double *UIntermediate;
-	double *U1;
-	double *V;
-	double *VIntermediate;
-	double *V1;
-	double *E;
-	double *EIntermediate;
-	double *E1;
-	double *P;
-	double *PIntermediate;
-	double *P1;
-	double *Ro;
-	double *Ro1;
+	double *U; //Solid phase (particles) velocity on current step
+	double *UIntermediate; //Solid phase (particles) velocity between steps
+	double *U1; //Solid phase (particles) velocity on next step
+	double *V; //Gas phase (particles) velocity on current step
+	double *VIntermediate; //Gas phase (particles) velocity between steps
+	double *V1; //Gas phase (particles) velocity on next step
+	double *E; //Gas phase (particles) energy on current step
+	double *EIntermediate; //Gas phase (particles) energy between steps
+	double *E1; //Gas phase (particles) energy on next step
+	double *P;  //Gas phase (particles) pressure on current step
+	double *PIntermediate; //Gas phase (particles) pressure between steps
+	double *P1; //Gas phase (particles) pressure on next step
+	double *Ro;  //Gas phase (particles) density on current step
+	double *Ro1;  //Gas phase (particles) density on next step
 	//Auxiliary arrays declaration
-	double *viscosity1;
-	double *viscosity2;
+	double *viscosity1; //Artificial viscosity
+	double *viscosity2; //Artificial viscosity
+	double *RightBorderMassFlow;
 public:
 	double DefineFlowDirection(int index, double* IntermVelocity, int Border, double* Value);
-	double DefineMassTranslatio(double ThisCellVelocity, double RightCellVelocity, double ThisCellDensity, double RightCellDensity, double dt);
+	double DefineRightBorderMassTranslation(double ThisCellVelocity, double RightCellVelocity, double ThisCellDensity, double RightCellDensity, double dt);
 	void MainProc();
 	LPM();
 	~LPM();
